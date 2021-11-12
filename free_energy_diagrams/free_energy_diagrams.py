4d4b38cfcae7c98f4370a655901b6656d87d24ba
warci626 <warci626@student.otago.ac.nz>
2021-11-13 09:29:56 +1300

    Move text

--- free_energy_diagrams/free_energy_diagrams.py
+++ free_energy_diagrams/free_energy_diagrams.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
+""" 
+This class is designed to produce free energy diagrams given a set of
+thermodynamic energy levels and kinetic barriers.
+
+"""
 
 import matplotlib.pyplot as plt
 import pdb
 import numpy as np
 from matplotlib.lines import Line2D as line
-from scipy.interpolate import interp1d
+
+
+
 
 class Free_Energy_Diagram():
-	"""
 	
-	
-	"""
     def __init__(self,  ):
