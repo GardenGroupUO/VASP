6340b3da1349f8e0d6c03e72085e4cf5019aca9f
warci626 <warci626@student.otago.ac.nz>
2021-11-12 14:38:51 +1300

    test

--- free_energy_diagrams/free_energy_diagrams.py
+++ free_energy_diagrams/free_energy_diagrams.py
@@ -6,6 +6,20 @@ import numpy as np
 from matplotlib.lines import Line2D as line
 from scipy.interpolate import interp1d
 
+class Free_Energy_Diagram():
+    def __init__(self,  ):
+
+
+
+
+
+
+
+
+
+
+
+
 state_len = 0.3
 state_pos = 0
 
