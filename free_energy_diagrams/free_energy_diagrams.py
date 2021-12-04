6d202bb8a3f607750e1b57c3975bc7f2bcfb8948
warci626 <warci626@student.otago.ac.nz>
2021-12-04 18:18:18 +1300

    added the add_level method

--- free_energy_diagrams/free_energy_diagrams.py
+++ free_energy_diagrams/free_energy_diagrams.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 """ 
-This class is designed to produce free energy diagrams given a set of
+This module is designed to produce free energy diagrams given a set of
 thermodynamic energy levels and kinetic barriers.
+
+It was heavily based off a script written by Giacomo Marchioro called
+PyEnergyDiagrams at their GitHub: giacomomarchioro.
 
 """
 
@@ -13,6 +16,80 @@ from matplotlib.lines import Line2D as line
 
 
 
-class Free_Energy_Diagram():
+class FED():
 	
-    def __init__(self,  ):
+
+    def __init__(self):
+        self.pos_number = 0
+        self.energies = []
+        self.positions = []
+        self.bottom_texts = []
+        self.top_texts = []
+        self.left_texts = []
+        self.right_texts = []
+        self.colours = []
+        self.links = []
+        
+
+
+    
+    def add_level(self, energy, bottom_text='', position=None, 
+                top_text='Energy', left_text='', right_text='', color='k'):
+        '''
+        This is a method of the FED class. It will save all of the data
+        for a new energy level into the lists required.
+
+        Parameters
+        __________
+
+        energy : int
+            The energy of the level in eV
+        bottom_text : str
+            The text underneath the level, ie the label. (default = '')
+        position : str
+            The position of the level in the plot. If it is left empty then the
+            level will be added after the last energy level. If position is last
+            or l, then the level will be added bellow the previous energy level.
+        top_text : str
+            Text above the energy level, defaults to 'Energy', the energy of the
+            level. (default = 'Energy')
+        left_text : str
+            The text to the left of the level. (default = '')
+        right_text : str
+            The text to the right of the level. (default = '')
+        color : str
+            Colour of the level. (default = 'k')
+
+        Returns
+        _______
+
+        Append all the data about the level in question to the relevant
+        class attributes. 
+
+        '''
+
+        if position is None:
+            position = self.pos_number + 1
+            self.pos_number += 1
+        elif position == 'last' or position == 'l':
+            position = self.pos_number
+        else:
+            raise ValueError(
+                "Position must be None or 'last' (abrv. 'l'. It was: %s" % position
+            )
+        
+        if top_text == 'Energy':
+            top_text = energy
+        
+        link = []
+        
+        self.energies.append(energy)
+        self.positions.append(position)
+        self.bottom_texts.append(bottom_text)
+        self.top_texts.append(top_text)
+        self.left_texts.append(left_text)
+        self.right_texts.append(right_text)
+        self.colours.append(color)
+        self.links.append(link)
+
+
